fdfd5f3398f1376587eb6fdac5cdbc8e88db0f75
Laura Gomez <laura@elenagoto.ch>
2024-11-13 22:05:58 +0100

    feat: display empty words grid and guesses

---
+++ src/components/Game/Guess.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { range } from '../../utils';
+
+function Guess({ word = null }) {
+  if (word) {
+    return (
+      <>
+        {word.split('').map((letter) => (
+          <span className='cell' key={crypto.randomUUID()}>
+            {letter}
+          </span>
+        ))}
+      </>
+    );
+  }
+
+  return (
+    <>
+      {range(5).map((num) => (
+        <span className='cell' key={crypto.randomUUID()}></span>
+      ))}
+    </>
+  );
+}
+
+export default Guess;
--- src/components/Game/GuessResults.js
+++ src/components/Game/GuessResults.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Guess from './Guess';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guesses }) {
+  let emptySlots = NUM_OF_GUESSES_ALLOWED - guesses.length;
+  console.log(guesses.length);
   return (
     <div className='guess-results'>
       {guesses.map(({ id, label }) => (
         <p className='guess' key={id} data-key={id}>
-          {label}
+          <Guess word={label} />
+        </p>
+      ))}
+      {range(emptySlots).map((num) => (
+        <p
+          className='guess'
+          key={crypto.randomUUID()}
+          data-key={crypto.randomUUID()}
+        >
+          <Guess />
         </p>
       ))}
     </div>
